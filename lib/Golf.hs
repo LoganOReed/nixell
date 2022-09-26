ac068c593777b846c8a8f0aa48e15447ce9299ee
orlogan <orlogan7@gmail.com>
2022-09-26 16:23:53 -0400

    Finished Homework Three

--- lib/Golf.hs
+++ lib/Golf.hs
@@ -5,5 +5,24 @@ module Golf where
 hop :: [a] -> Int -> [a]
 hop str n = map fst $ filter (\x -> snd x `mod` n == 0) (zip str [1..]) 
 
-skip :: [a] -> [[a]]
-skip str = map (hop str) [1 .. length(str)]
+skips :: [a] -> [[a]]
+skips str = map (hop str) [1 .. length(str)]
+
+-- localMax :: [Integer] -> [Integer]
+--
+-- localMin :: [Integer] -> [Integer]
+
+checkMax :: Integer -> Integer -> Integer -> [Integer]
+checkMax a b c 
+  | a < b && b > c  = [b]
+  | otherwise       = []
+
+checkMin :: Integer -> Integer -> Integer -> [Integer]
+checkMin a b c 
+  | a > b && b < c  = [b]
+  | otherwise       = []
+
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (a:b:c:xs) = (checkMax a b c) ++ (checkMin a b c) ++ localMaxima (b:c:xs)
+localMaxima _          = []
