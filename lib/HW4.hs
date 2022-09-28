c0e5764d944c08808b3c6ba30027848b83bd7767
orlogan <orlogan7@gmail.com>
2022-09-27 23:37:05 -0400

    Completed First Part of HW4

---
+++ lib/HW4.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module HW4 (
+
+)where
+
+-- Multiplies all even numbers less 2
+fun1 :: [Integer] -> Integer
+fun1 []       = 1
+fun1 (x:xs)
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = foldl (*) 1 . map (\x -> x - 2) . filter even
+
+-- Collatz conjecture
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
