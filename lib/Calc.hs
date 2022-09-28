ea7a47bb6b788e972f3aa0a6100f37856be98f8c
orlogan <orlogan7@gmail.com>
2022-09-28 03:29:17 -0400

    Finished Basic Calculator

---
+++ lib/Calc.hs
@@ -0,0 +1,70 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Calc (
+
+)where
+
+import ExprT
+import Parser
+
+newtype MinMax = MinMax Integer deriving (Ord, Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a 
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where 
+  lit     = id
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x 
+    | x <= 0   = False
+    |otherwise = True
+  add x y = x || y
+  mul x y = x && y
+
+instance Expr MinMax where
+  lit x   = MinMax x
+  add x y 
+    | x <= y    = x
+    | otherwise = y
+  mul x y 
+    | x > y     = x
+    | otherwise = y
+
+instance Expr Mod7 where
+  lit x   = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7) 
+
+
+eval :: ExprT -> Integer
+eval (Lit n) = n
+eval (Add n1 n2) = (eval n1) + (eval n2)
+eval (Mul n1 n2) = (eval n1) * (eval n2)
+
+checkEval :: Maybe ExprT -> Maybe Integer
+checkEval (Just e)  = Just (eval e)
+checkEval (Nothing) = Nothing
+
+evalStr :: String -> Maybe Integer
+evalStr = checkEval . (parseExp Lit Add Mul) 
+
+reify :: ExprT -> ExprT
+reify = id
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
