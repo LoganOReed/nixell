89b41de7b2708a9b400fe6873016dfd9c281a68f
orlogan <orlogan7@gmail.com>
2022-09-29 20:29:25 -0400

    Fixed indexJ

--- lib/HWSeven/JoinList.hs
+++ lib/HWSeven/JoinList.hs
@@ -5,7 +5,7 @@ module HWSeven.JoinList (
 
 )where
 
-import Data.Monoid
+--import Data.Monoid
 import HWSeven.Sized
 
 data JoinList m a = Empty
@@ -24,11 +24,37 @@ tag _              = mempty
 indexJ :: (Sized b, Monoid b) => 
           Int -> JoinList b a -> Maybe a
 indexJ _ Empty        = Nothing
-indexJ n (Single _ a) = Just a
+indexJ _ (Single _ a) = Just a
 indexJ n (Append s jl1 jl2)
-  | n > sizeS   = Nothing
-  | n < s1      = indexJ (s1 - n) jl1
+  | n >= sizeS  = Nothing
+  | n < s1      = indexJ n jl1
   | otherwise   = indexJ (n - s1) jl2
   where sizeS  = getSize $ size s
         s1     = getSize . size $ tag jl1
-        
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+-- For Testing
+newtype Sum n = Sum n
+  deriving (Eq, Ord, Show)
+
+instance Num n => Semigroup (Sum n) where
+  Sum x <> Sum y = Sum (x + y)
+
+instance Num n => Monoid (Sum n) where
+  mempty = Sum 0
+
+newtype Product n = Product n
+  deriving (Eq, Ord, Show)
+
+instance Num n => Semigroup (Product n) where
+  Product x <> Product y = Product (x * y)
+
+instance Num n => Monoid (Product n) where
+  mempty = Product 1
+
+-- (Single (Size 1) 'h') +++ (Single (Size 1) 'e') +++ (Single (Size 1) 'l') +++ (Single (Size 1) 'l') +++ (Single (Size 1) 'o')
+-- ( Append (Size 5) (Append (Size 3) (Append (Size 2) (Single (Size 1) 'a') (Single (Size 1) 'b') ) (Single (Size 1) 'c')) (Append (Size 2) (Single (Size 1) 'd') (Single (Size 1) 'e')))
